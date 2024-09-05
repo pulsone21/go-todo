9336b6e30e3dd1b089d1fc3a5fa5f4d927a6ac38
Johannes Böhm <pulsone21@freenet.de>
2024-09-05 22:23:31 +0200

    Updated Config with new params, dependency updates

--- cmd/config.go
+++ cmd/config.go
@@ -2,25 +2,55 @@ package cmd
 
 import (
 	"fmt"
+	"os"
 	"strings"
 
 	"github.com/pulsone21/go-todo/internal/datahandler"
+	"github.com/pulsone21/go-todo/internal/logg"
 )
 
 type Config struct {
 	DataHandler string `yaml:"data_handler"`
 	StoragePath string `yaml:"storage_path"`
-	minWidth    int    `yaml:"min_width"`
-	tabWidth    int    `yaml:"tab_width"`
-	padChar     byte   `yaml:"pad_char"`
-	padding     int    `yaml:"padding"`
+	MinWidth    int    `yaml:"min_width"`
+	TabWidth    int    `yaml:"tab_width"`
+	PadChar     string `yaml:"pad_char"`
+	Padding     int    `yaml:"padding"`
+	LogPath     string `yaml:"log_path"`
+	LogLevel    int    `yaml:"log_level"`
+	LogSize     int    `yaml:"log_size"`
 }
 
-func (c Config) CreateHandler() (datahandler.Handler, error) {
+func NewConfig() Config {
+	home_dir := os.Getenv("$HOME")
+	return Config{
+		DataHandler: "CSV",
+		StoragePath: fmt.Sprintf("%v/.config/todo/data", home_dir),
+		MinWidth:    2,
+		TabWidth:    4,
+		PadChar:     " ",
+		Padding:     4,
+		LogPath:     fmt.Sprintf("%v/.config/todo", home_dir),
+		LogLevel:    0,
+		LogSize:     20,
+	}
+}
+
+func (c *Config) CreateHandler() (datahandler.Handler, error) {
+	path := ensure_path(c.StoragePath)
+	logg.Info(fmt.Sprintf("ensured path: %v\n", path))
 	switch strings.ToUpper(c.DataHandler) {
 	case "CSV":
-		return datahandler.NewCSVLoader(c.StoragePath), nil
+		return datahandler.NewCSVLoader(path), nil
 	default:
 		return nil, fmt.Errorf("error in datahandler creation, %s is not implemented.", c.DataHandler)
 	}
+}
+
+func ensure_path(path string) string {
+	str_le := len(path)
+	for strings.HasSuffix(path, "/") {
+		path = path[:str_le-1]
+	}
+	return path
 }
